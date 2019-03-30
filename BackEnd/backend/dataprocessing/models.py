83e53aeaa990c7283991e288ec9ef884773f2636
AbdAyyad <abdayyad2012@gmail.com>
2019-03-30 16:16:50 +0300

    enhance data base

--- BackEnd/backend/dataprocessing/models.py
+++ BackEnd/backend/dataprocessing/models.py
@@ -23,14 +23,26 @@ class Patient(AbstractBaseUser):
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
     question_body = models.TextField(max_length=300)
-    choice_a = models.TextField(max_length=30)
-    choice_b = models.TextField(max_length=30)
-    choice_c = models.TextField(max_length=30)
-    choice_d = models.TextField(max_length=30)
-    answer = models.TextField(max_length=1)
+    correct_answer = models.IntegerField()
     quiz = models.ForeignKey('Quiz', on_delete=models.CASCADE)
+
+
+class Choice(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField(max_length=30)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
 
 
 class Quiz(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.TextField(max_length=30)
+    dr = models.ForeignKey('Dr', on_delete=models.CASCADE)
+
+
+class Test(models.Model):
+    patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
+
+
+class Answer(models.Model):
+    test = models.ForeignKey('Test', on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
